c017ab7c45fc91db66bdd72c8f12aec36d006080
tyomzya <tyomzya@mail.ru>
2024-06-22 16:35:19 +0300

    Portfolio block

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
-var welcome = "Hello";
-console.log(welcome)
+document.getElementById('buttonDesign').onclick = function() {
+    document.getElementById('portfolioDesign').hidden = false;
+    document.getElementById('portfolioDev').hidden = true;
+    document.getElementById('test').className = link;
+}
+
+document.getElementById('buttonDev').onclick = function() {
+    document.getElementById('portfolioDesign').hidden = true;
+    document.getElementById('portfolioDev').hidden = false;
+}
