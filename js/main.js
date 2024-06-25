0e36544bc67262f8a050311c1b8e7d85938fc107
tyomzya <tyomzya@mail.ru>
2024-06-25 17:40:19 +0300

    Updated portfolio block

--- js/main.js
+++ js/main.js
@@ -1,10 +1,23 @@
 document.getElementById('buttonDesign').onclick = function() {
+
+    document.getElementById('buttonDesign').style.cssText = 'background-color: #21A049; border-radius: 15px; border: #21A049 solid'
+    document.getElementById('buttonDesignValue').style.cssText = 'color: #202020'
+
+    document.getElementById('buttonDev').style.cssText = 'background-color: transparent; border-radius: 15px; border: #21A049 solid'
+    document.getElementById('buttonDevValue').style.cssText = 'color: #21A049'
+
     document.getElementById('portfolioDesign').hidden = false;
     document.getElementById('portfolioDev').hidden = true;
-    document.getElementById('test').className = link;
 }
 
 document.getElementById('buttonDev').onclick = function() {
+
+    document.getElementById('buttonDesign').style.cssText = 'background-color: transparent; border-radius: 15px; border: #21A049 solid'
+    document.getElementById('buttonDesignValue').style.cssText = 'color: #21A049'
+
+    document.getElementById('buttonDev').style.cssText = 'background-color: #21A049; border-radius: 15px; border: #21A049 solid'
+    document.getElementById('buttonDevValue').style.cssText = 'color: #202020'
+
     document.getElementById('portfolioDesign').hidden = true;
     document.getElementById('portfolioDev').hidden = false;
 }
