0b5f4164a65af2cefcf828bda32266e042d77ad2
tyomzya <tyomzya@mail.ru>
2024-06-26 14:00:54 +0300

    Design Portfolio filled

--- js/main.js
+++ js/main.js
@@ -23,3 +23,52 @@ document.getElementById('buttonDev').onclick = function() {
     document.getElementById('portfolioDesign').hidden = true;
     document.getElementById('portfolioDev').hidden = false;
 }
+
+//Обработчик кнопки Дизайн-Логотипы
+document.getElementById('buttonLogos').onclick = function() {
+
+    document.getElementById('buttonLogos').style.cssText = 'background-color: #21A049;'
+    document.getElementById('buttonLogosValue').style.cssText = 'color: #202020'
+
+    document.getElementById('buttonBanners').style.cssText = 'background-color: transparent;'
+    document.getElementById('buttonBannersValue').style.cssText = 'color: #21A049'
+
+     document.getElementById('buttonTours').style.cssText = 'background-color: transparent;'
+    document.getElementById('buttonToursValue').style.cssText = 'color: #21A049'
+
+    document.getElementById('portfolioLogosID').hidden = false;
+    document.getElementById('portfolioBannersID').hidden = true;
+    document.getElementById('portfolioToursID').hidden = true;
+}
+//Обработчик кнопки Дизайн-Баннеры
+document.getElementById('buttonBanners').onclick = function() {
+
+    document.getElementById('buttonLogos').style.cssText = 'background-color: transparent;'
+    document.getElementById('buttonLogosValue').style.cssText = 'color: #21A049'
+
+    document.getElementById('buttonBanners').style.cssText = 'background-color: #21A049;'
+    document.getElementById('buttonBannersValue').style.cssText = 'color: #202020'
+
+     document.getElementById('buttonTours').style.cssText = 'background-color: transparent;'
+    document.getElementById('buttonToursValue').style.cssText = 'color: #21A049'
+
+    document.getElementById('portfolioLogosID').hidden = true;
+    document.getElementById('portfolioBannersID').hidden = false;
+    document.getElementById('portfolioToursID').hidden = true;
+}
+//Обработчик кнопки Дизайн-Баннеры
+document.getElementById('buttonTours').onclick = function() {
+
+    document.getElementById('buttonLogos').style.cssText = 'background-color: transparent;'
+    document.getElementById('buttonLogosValue').style.cssText = 'color: #21A049'
+
+    document.getElementById('buttonBanners').style.cssText = 'background-color: transparent;'
+    document.getElementById('buttonBannersValue').style.cssText = 'color: #21A049'
+
+     document.getElementById('buttonTours').style.cssText = 'background-color: #21A049;'
+    document.getElementById('buttonToursValue').style.cssText = 'color: #202020'
+
+    document.getElementById('portfolioLogosID').hidden = true;
+    document.getElementById('portfolioBannersID').hidden = true;
+    document.getElementById('portfolioToursID').hidden = false;
+}
